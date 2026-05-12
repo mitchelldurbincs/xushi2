2976b9457f0594ab709a978396e0954ba0f7eeaa
mitchelldurbincs <68557654+mitchelldurbincs@users.noreply.github.com>
2026-05-12 09:35:16 -0400

    Refactor bot behavior helpers into internal primitives module

---
+++ src/bots/src/internal/behavior_primitives.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <xushi2/sim/match_state.hpp>
+
+#include <xushi2/common/action_canon.hpp>
+
+namespace xushi2::bots::internal {
+
+const sim::HeroState* find_opponent(const sim::MatchState& state,
+                                    const sim::HeroState& self);
+
+float aim_delta_toward(const sim::HeroState& self, float tx, float ty);
+
+common::Action walk_to_objective(const sim::HeroState& self);
+
+common::Action hold_and_shoot(const sim::MatchState& state,
+                              const sim::HeroState& self);
+
+}  // namespace xushi2::bots::internal
